0e7a39c219691a96614f39dea4bf66afdcf14de2
Blessing <blessingogbogu@gmail.com>
2025-02-07 19:56:29 +0000

    debug setup

--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@ let switchOff = document.getElementById('switchoff');
 let light = document.getElementById('light');
 
 switchOn.addEventListener('click', function () {
-light.src = 'pic_bulbon.gif';
+light.src = 'src/pic_bulbon.gif';
 light.style.width = '300px';
 light.style.height = '500px';
-alert('button on');
 });
 
 switchOff.addEventListener('click', function () {
-    light.src = 'pic_bulboff.jpeg';
+    light.src = 'src/pic_bulboff.jpeg';
     light.style.width = '300px';
     light.style.height = '500px';
     });
