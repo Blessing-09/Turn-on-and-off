8961c913a652d2c925e8243dbf280b39c796d1cb
Blessing <blessingogbogu@gmail.com>
2025-02-08 01:10:34 +0000

    Features update

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ let switchOn = document.getElementById('switchon');
 let switchOff = document.getElementById('switchoff');
 let light = document.getElementById('light');
 
-switchOn.addEventListener('click', function () {
-light.src = 'src/pic_bulbon.gif';
-light.style.width = '300px';
-light.style.height = '500px';
-});
+//Working on using more functions to declare codes for increased reusability, as well as to
+//  make them more readeable and efficient
+let picOn = 'src/pic_bulbon.gif';
+let picOff = 'src/pic_bulboff.jpeg';
 
-switchOff.addEventListener('click', function () {
-    light.src = 'src/pic_bulboff.jpeg';
-    light.style.width = '300px';
-    light.style.height = '500px';
+function bulbSwitch(bulb, item, img) {
+    bulb.addEventListener('click', function() {
+        item.src = img;
     });
+}
+bulbSwitch(switchOn, light, picOn);
+bulbSwitch(switchOff, light, picOff);
+
+// switchOn.addEventListener('click', function () {
+// light.src = 'src/pic_bulbon.gif';
+// light.style.width = '300px';
+// light.style.height = '500px';
+// });
+
+// switchOff.addEventListener('click', function () {
+//     light.src = 'src/pic_bulboff.jpeg';
+//     light.style.width = '300px';
+//     light.style.height = '500px';
+//     });
